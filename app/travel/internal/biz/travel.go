866ffb7b5a7df4bd0f53948cf5dfed3c2790dc39
symbolF <864911582@qq.com>
2024-12-09 10:20:06 +0800

    refactor:调整

--- app/travel/internal/biz/travel.go
+++ app/travel/internal/biz/travel.go
@@ -1,8 +1,14 @@
 package biz
 
 import (
+	pb "blog/api/account/v1"
 	"blog/api/global"
+	v1 "blog/api/travel/v1"
+	"blog/internal/constx"
 	"context"
+	"errors"
+	"math/rand"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
 )
@@ -74,8 +80,132 @@ type TravelUsecase struct {
 func NewTravelUsecase(repo TravelRepo, logger log.Logger) *TravelUsecase {
 	return &TravelUsecase{repo: repo, log: log.NewHelper(logger)}
 }
-
-// CreateTravel creates a Travel, and returns the new Travel.
-func (uc *TravelUsecase) CreateTravel(ctx context.Context, g *Travel) (*Travel, error) {
-
+func (o *TravelUsecase) Save(ctx context.Context, req *v1.SaveTravelRequest) (*global.Empty, error) {
+	if req.Title == "" {
+		return nil, errors.New("title is empty")
+	}
+	u, err := constx.DefaultUser.User(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &global.Empty{}, o.repo.Save(ctx, &TravelBase{
+		Id:          int(req.Id),
+		Title:       req.Title,
+		IsHidden:    req.IsHidden,
+		Description: req.Description,
+		Video:       req.Video,
+		Photos:      req.Photo,
+		AccountId:   int(u.Id),
+	})
+}
+func (o *TravelUsecase) Get(ctx context.Context, req *global.ID) (*v1.GetTravelReply, error) {
+	u := constx.DefaultUser.Default(ctx)
+	info, err := o.repo.Get(ctx, &TravelBase{
+		Id:        int(req.Id),
+		AccountId: int(u.Id),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetTravelReply{
+		Id:          int64(info.Id),
+		Title:       info.Title,
+		IsHidden:    info.IsHidden,
+		Description: info.Description,
+		Video:       info.Video,
+		Photos:      info.Photos,
+		BrowseNum:   int64(info.BrowseNum),
+		ThumbNum:    int64(info.ThumbNum),
+		IsThumb:     info.IsThumb,
+		CollectNum:  int64(info.CollectNum),
+		IsCollect:   info.IsCollect,
+		AccountId:   int64(info.AccountId),
+		Account: &pb.AccountInfoReply{
+			Id:       int64(info.AccountId),
+			Nickname: info.Nickname,
+			Avatar:   info.Avatar,
+		},
+		CreatedAt: info.CreatedAt,
+		UpdatedAt: info.UpdatedAt,
+	}, nil
+}
+func (o *TravelUsecase) List(ctx context.Context, req *v1.ListTravelRequest) (*v1.ListTravelReply, error) {
+	u := constx.DefaultUser.Default(ctx)
+	total, list, err := o.repo.List(ctx, &TravelQuery{
+		PageInfo:    req.Page,
+		Title:       req.Title,
+		AccountId:   u.Id,
+		Description: req.Description,
+		My:          req.My,
+		MyCollect:   req.MyCollect,
+		MyThumb:     req.MyThumb,
+		Sort:        req.Sort,
+	})
+	if err != nil {
+		return nil, err
+	}
+	resp := &v1.ListTravelReply{
+		Total: total,
+		List:  make([]*v1.ListTravel, 0, len(list)),
+	}
+	_rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for _, info := range list {
+		one := &v1.ListTravel{
+			Id:          int64(info.Id),
+			Title:       info.Title,
+			Description: info.Description,
+			Video:       info.Video,
+			BrowseNum:   int64(info.BrowseNum),
+			ThumbNum:    int64(info.ThumbNum),
+			IsThumb:     info.IsThumb,
+			CollectNum:  int64(info.CollectNum),
+			IsCollect:   info.IsCollect,
+			AccountId:   int64(info.AccountId),
+			Account: &pb.AccountInfoReply{
+				Id:       int64(info.AccountId),
+				Nickname: info.Nickname,
+				Avatar:   info.Avatar,
+			},
+			CreatedAt: info.CreatedAt,
+			UpdatedAt: info.UpdatedAt,
+		}
+		if len(info.Photos) > 0 {
+			index := _rand.Intn(len(info.Photos))
+			one.Photo = info.Photos[index]
+		}
+		resp.List = append(resp.List, one)
+	}
+	return resp, nil
+}
+func (o *TravelUsecase) Delete(ctx context.Context, req *global.ID) (*global.Empty, error) {
+	u, err := constx.DefaultUser.User(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &global.Empty{}, o.repo.Delete(ctx, &TravelBase{
+		Id:        int(req.Id),
+		AccountId: int(u.Id),
+	})
+}
+func (o *TravelUsecase) Thumb(ctx context.Context, req *global.Action) (*global.Empty, error) {
+	u, err := constx.DefaultUser.User(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &global.Empty{}, o.repo.Thumb(ctx, &TravelDo{
+		Id:        int(req.Id),
+		AccountId: int(u.Id),
+		Do:        req.GetDo(),
+	})
+}
+func (o *TravelUsecase) Collect(ctx context.Context, req *global.Action) (*global.Empty, error) {
+	u, err := constx.DefaultUser.User(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &global.Empty{}, o.repo.Collect(ctx, &TravelDo{
+		Id:        int(req.Id),
+		AccountId: int(u.Id),
+		Do:        req.GetDo(),
+	})
 }
--- app/user/internal/biz/user.go
+++ app/user/internal/biz/user.go
@@ -5,7 +5,10 @@ import (
 	v1 "blog/api/user/v1"
 	"blog/internal/constx"
 	"context"
+	"errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"math/rand"
+	"time"
 )
 
 // User is a User model.
@@ -24,7 +27,7 @@ type User struct {
 	UpdatedAt    int64
 }
 type ProjectQuery struct {
-	global.PageInfo
+	*global.PageInfo
 	ExperienceId int
 	UserId       int
 	Title        string
@@ -46,7 +49,7 @@ type Project struct {
 	Photos       []string
 }
 type ExperienceQuery struct {
-	global.PageInfo
+	*global.PageInfo
 	UserId  int
 	Company string
 }
@@ -88,79 +91,229 @@ type UserUsecase struct {
 	log  *log.Helper
 }
 
-// NewUserUsecase new a User usecase.
 func NewUserUsecase(repo UserRepo, logger log.Logger) *UserUsecase {
 	return &UserUsecase{repo: repo, log: log.NewHelper(logger)}
 }
 
-// CreateUser creates a User, and returns the new User.
-func (uc *UserUsecase) CreateUser(ctx context.Context, req *v1.SaveUserRequest) error {
-	u, err := constx.DefaultUser.User(ctx)
-	if err != nil {
-		return err
+func (uc *UserUsecase) SaveUser(ctx context.Context, req *v1.SaveUserRequest) (*global.Empty, error) {
+	if req.Name == "" {
+		return nil, errors.New("姓名必须")
 	}
-	return uc.repo.SaveUser(ctx)
-}
-func (uc *UserUsecase) GetUser(ctx context.Context, g *User) (*User, error) {
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return uc.repo.GetUser(ctx, g)
+	return &global.Empty{}, uc.repo.SaveUser(ctx, &User{
+		ID:           int(u.Id),
+		Name:         req.Name,
+		Email:        req.Email,
+		Avatar:       req.Avatar,
+		Professional: req.Professional,
+		Skills:       req.Skills,
+		Description:  req.Description,
+		Address:      req.Address,
+	})
 }
-func (uc *UserUsecase) SaveProject(ctx context.Context, g *Project) error {
-	u, err := constx.DefaultUser.User(ctx)
-	if err != nil {
-		return err
-	}
-	return uc.repo.SaveProject(ctx, g)
-}
-func (uc *UserUsecase) GetProject(ctx context.Context, g *Project) (*Project, error) {
+func (uc *UserUsecase) GetUser(ctx context.Context) (*v1.GetUserReply, error) {
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return uc.repo.GetProject(ctx, g)
+	info, err := uc.repo.GetUser(ctx, &User{ID: int(u.Id)})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetUserReply{
+		Id:           int64(info.ID),
+		Name:         info.Name,
+		Email:        info.Email,
+		Avatar:       info.Avatar,
+		Professional: info.Professional,
+		Skills:       info.Skills,
+		Description:  info.Description,
+		Address:      info.Address,
+		Experience:   int64(info.Experience),
+		Project:      int64(info.Project),
+		CreatedAt:    info.CreatedAt,
+		UpdatedAt:    info.UpdatedAt,
+	}, nil
 }
-func (uc *UserUsecase) DeleteProject(ctx context.Context, g *Project) error {
+func (uc *UserUsecase) SaveProject(ctx context.Context, req *v1.SaveProjectRequest) (*global.Empty, error) {
+	if req.ExperienceId == 0 || req.Start == 0 || len(req.Title) == 0 {
+		return nil, errors.New("参数丢失")
+	}
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return uc.repo.DeleteProject(ctx, g)
+	return &global.Empty{}, uc.repo.SaveProject(ctx, &Project{
+		ID:           int(req.Id),
+		UserId:       int(u.Id),
+		ExperienceId: int(req.ExperienceId),
+		Title:        req.Title,
+		Description:  req.Description,
+		Skills:       req.Skills,
+		Start:        req.Start,
+		End:          req.End,
+		Link:         req.Link,
+		Photos:       req.Photos,
+	})
 }
-func (uc *UserUsecase) ListProject(ctx context.Context, g *ProjectQuery) (int, []*Project, error) {
+func (uc *UserUsecase) GetProject(ctx context.Context, req *global.ID) (*v1.GetProjectReply, error) {
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return uc.repo.ListProject(ctx, g)
+	info, err := uc.repo.GetProject(ctx, &Project{ID: int(req.Id), UserId: int(u.Id)})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetProjectReply{
+		Id:           int64(info.ID),
+		ExperienceId: int64(info.ExperienceId),
+		Title:        info.Title,
+		Description:  info.Description,
+		Skills:       info.Skills,
+		Start:        info.Start,
+		End:          info.End,
+		Link:         info.Link,
+		Photos:       info.Photos,
+		CreatedAt:    info.CreatedAt,
+		UpdatedAt:    info.UpdatedAt,
+		UserId:       int64(info.UserId),
+	}, nil
 }
-func (uc *UserUsecase) SaveExperience(ctx context.Context, g *Experience) error {
+func (uc *UserUsecase) DeleteProject(ctx context.Context, req *global.ID) (*global.Empty, error) {
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return uc.repo.SaveExperience(ctx, g)
+	return &global.Empty{}, uc.repo.DeleteProject(ctx, &Project{ID: int(req.Id), UserId: int(u.Id)})
 }
-func (uc *UserUsecase) GetExperience(ctx context.Context, g *Experience) (*Experience, error) {
+func (uc *UserUsecase) ListProject(ctx context.Context, req *v1.ListProjectRequest) (*v1.ListProjectReply, error) {
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return uc.repo.GetExperience(ctx, g)
+	total, list, err := uc.repo.ListProject(ctx, &ProjectQuery{
+		PageInfo:     req.Page,
+		ExperienceId: int(req.ExperienceId),
+		UserId:       int(u.Id),
+		Title:        req.Title,
+		Skills:       req.Skills,
+		Time:         req.Time,
+	})
+	if err != nil {
+		return nil, err
+	}
+	resp := &v1.ListProjectReply{
+		List:  make([]*v1.ListProject, 0, len(list)),
+		Total: int64(total),
+	}
+	_rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for _, info := range list {
+		one := &v1.ListProject{
+			Id:           int64(info.ID),
+			ExperienceId: int64(info.ExperienceId),
+			Title:        info.Title,
+			Skills:       info.Skills,
+			Start:        info.Start,
+			End:          info.End,
+			Link:         info.Link,
+			UpdatedAt:    info.UpdatedAt,
+			UserId:       int64(info.UserId),
+		}
+		if len(info.Photos) > 0 {
+			index := _rand.Intn(len(info.Photos))
+			one.Photo = info.Photos[index]
+		}
+		resp.List = append(resp.List, one)
+	}
+	return resp, nil
 }
-func (uc *UserUsecase) DeleteExperience(ctx context.Context, g *Experience) error {
+func (uc *UserUsecase) SaveExperience(ctx context.Context, req *v1.SaveExperienceRequest) (*global.Empty, error) {
+	if req.Start == 0 || len(req.Company) == 0 {
+		return nil, errors.New("参数丢失")
+	}
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return uc.repo.DeleteExperience(ctx, g)
+	return &global.Empty{}, uc.repo.SaveExperience(ctx, &Experience{
+		ID:               int(req.Id),
+		UserId:           int(u.Id),
+		Company:          req.Company,
+		Role:             req.Role,
+		Location:         req.Location,
+		Start:            req.Start,
+		End:              req.End,
+		Description:      req.Description,
+		Responsibilities: req.Responsibilities,
+		Achievements:     req.Achievements,
+		Skills:           req.Skills,
+	})
 }
-func (uc *UserUsecase) ListExperience(ctx context.Context, g *ExperienceQuery) (int, []*Experience, error) {
+func (uc *UserUsecase) GetExperience(ctx context.Context, req *global.ID) (*v1.GetExperienceReply, error) {
 	u, err := constx.DefaultUser.User(ctx)
 	if err != nil {
-		return
+		return nil, err
 	}
-	return uc.repo.ListExperience(ctx, g)
+	info, err := uc.repo.GetExperience(ctx, &Experience{ID: int(req.Id), UserId: int(u.Id)})
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetExperienceReply{
+		Id:               int64(info.ID),
+		Company:          info.Company,
+		Role:             info.Role,
+		Location:         info.Location,
+		Start:            info.Start,
+		End:              info.End,
+		Description:      info.Description,
+		Responsibilities: info.Responsibilities,
+		Achievements:     info.Achievements,
+		Skills:           info.Skills,
+		CreatedAt:        info.CreatedAt,
+		UpdatedAt:        info.UpdatedAt,
+		UserId:           int64(info.UserId),
+	}, nil
+}
+func (uc *UserUsecase) DeleteExperience(ctx context.Context, req *global.ID) (*global.Empty, error) {
+	u, err := constx.DefaultUser.User(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &global.Empty{}, uc.repo.DeleteExperience(ctx, &Experience{ID: int(req.Id), UserId: int(u.Id)})
+}
+func (uc *UserUsecase) ListExperience(ctx context.Context, req *v1.ListExperienceRequest) (*v1.ListExperienceReply, error) {
+	u, err := constx.DefaultUser.User(ctx)
+	if err != nil {
+		return nil, err
+	}
+	total, list, err := uc.repo.ListExperience(ctx, &ExperienceQuery{
+		PageInfo: req.Page,
+		UserId:   int(u.Id),
+		Company:  req.Company,
+	})
+	if err != nil {
+		return nil, err
+	}
+	resp := &v1.ListExperienceReply{
+		Total: int64(total),
+		List:  make([]*v1.ListExperience, 0, len(list)),
+	}
+	for _, info := range list {
+		resp.List = append(resp.List, &v1.ListExperience{
+			Id:          int64(info.ID),
+			Company:     info.Company,
+			Role:        info.Role,
+			Location:    info.Location,
+			Start:       info.Start,
+			End:         info.End,
+			Description: info.Description,
+			UpdatedAt:   info.UpdatedAt,
+			UserId:      int64(info.UserId),
+		})
+	}
+	return resp, nil
 }
